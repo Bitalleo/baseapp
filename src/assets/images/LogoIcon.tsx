2eed306f497368391d78f69db1770aa3224f1ab3
FlowingRedstone <43510195+FlowingRedstone@users.noreply.github.com>
2022-02-15 22:26:45 -0700

    Update LogoIcon.tsx

--- src/assets/images/LogoIcon.tsx
+++ src/assets/images/LogoIcon.tsx
@@ -4,4 +4,8 @@ interface LogoIconProps {
     className?: string;
 }
 
-export const LogoIcon: React.FC<LogoIconProps> = (props: LogoIconProps) => ();
+export const LogoIcon: React.FC<LogoIconProps> = (props: LogoIconProps) => (
+    <svg className={props.className} xmlns="http://www.w3.org/2000/svg" width="60" height="100" viewBox="0 0 60 100">
+    <path class="cls-1" d="M37.4,0.019H0.118V41.972L24.932,64.355V23.639H38.181c8.421,0,12.575,4.049,12.575,10.573V65.367c0,6.523-3.93,10.91-12.575,10.91H0.005v23.732H37.283c19.986,0.113,38.737-9.9,38.737-32.842V33.649C76.132,10.254,57.381.019,37.4,0.019h0Z" transform="translate(0 -0.031)" fill="#FFF"/>
+  </svg>
+);
